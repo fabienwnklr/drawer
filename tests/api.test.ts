60c05ebf5049bb25ac42fb098e9d7ce5443b0d99
Fabien Winkler <fabien.winkler@outlook.fr>
2023-09-29 12:03:22 +0200

    🧪 adding test

---
+++ tests/api.test.ts
@@ -0,0 +1,84 @@
+import { describe, expect, it, beforeAll } from 'vitest';
+import { Drawer } from '../src/Drawer';
+import { Blob } from 'node:buffer';
+
+declare global {
+  interface Window {
+    Blob: any;
+  }
+}
+
+beforeAll(() => {
+  window.Blob = Blob; // use Node.js Blob instead of Jsdom's Blob
+});
+
+describe('API', () => {
+  document.body.innerHTML = `<div id="test"></div>`;
+
+  it('custom toolbar', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, {
+      defaultToolbar: false,
+      autoSave: false,
+    });
+    drawer.addToolbar();
+    drawer.addUndoBtn();
+    drawer.addRedoBtn();
+    drawer.addBrushBtn();
+    drawer.addEraserBtn();
+
+    expect(drawer.$toolbar).toBeInstanceOf(HTMLDivElement);
+    expect(drawer.$toolbar.querySelectorAll('button').length).toEqual(4);
+  });
+
+  it('setSize', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement);
+
+    drawer.setSize(500, 600);
+    expect(drawer.$canvas.width).toEqual(500);
+    expect(drawer.$canvas.height).toEqual(600);
+    // check max-width update of toolbar
+    expect(drawer.$toolbar.style.maxWidth).toEqual('500px');
+  });
+
+  it('setLineWidth', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement);
+
+    drawer.setLineWidth(10);
+    expect(drawer.ctx.lineWidth).toEqual(10);
+  });
+
+  it('changeTool', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement);
+
+    drawer.changeTool('eraser');
+    expect(drawer.activeTool).toEqual('eraser');
+    expect(drawer.$eraserBtn?.classList.contains('active')).toEqual(true);
+  });
+
+  it('setColor', async () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement);
+
+    await drawer.setColor('#3e3e3e');
+    expect(drawer.options.color).toEqual('#3e3e3e');
+    expect(drawer.$colorPicker?.value).toEqual('#3e3e3e');
+    expect(drawer.ctx.fillStyle).toEqual('#3e3e3e');
+
+    // With keyword, converted to hex
+    await drawer.setColor('red');
+    expect(drawer.options.color).toEqual('red');
+    expect(drawer.$colorPicker?.value).toEqual('red');
+    expect(drawer.ctx.fillStyle).toEqual('#ff0000');
+
+    // With rgb
+    drawer.setColor('rgb(0, 0, 152)');
+    expect(drawer.options.color).toEqual('rgb(0, 0, 152)');
+    expect(drawer.$colorPicker?.value).toEqual('rgb(0, 0, 152)');
+    expect(drawer.ctx.fillStyle).toEqual('#000098');
+
+    // With rgba
+    drawer.setColor('rgba(0, 0, 152, 0.5)');
+    expect(drawer.options.color).toEqual('rgba(0, 0, 152, 0.5)');
+    expect(drawer.$colorPicker?.value).toEqual('rgba(0, 0, 152, 0.5)');
+    expect(drawer.ctx.fillStyle).toEqual('rgba(0, 0, 152, 0.5)');
+  });
+});
--- tests/drawer.test.ts
+++ tests/drawer.test.ts
@@ -4,7 +4,7 @@ import { Blob } from 'node:buffer';
 
 declare global {
   interface Window {
-    Blob: any
+    Blob: any;
   }
 }
 
@@ -16,7 +16,8 @@ describe('Drawer', () => {
   document.body.innerHTML = `<div id="test"></div>`;
 
   it('Init single basic drawer', () => {
-    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement);
+
     expect(drawer).toBeInstanceOf(Drawer);
     expect(drawer.$toolbar).toBeInstanceOf(HTMLDivElement);
     expect(drawer.$toolbar.querySelectorAll('button, input').length).not.toEqual(0);
@@ -28,6 +29,7 @@ describe('Drawer', () => {
       defaultToolbar: false,
       autoSave: false,
     });
+
     expect(drawer.$toolbar).toBeUndefined();
   });
 
@@ -35,22 +37,7 @@ describe('Drawer', () => {
     const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement);
 
     expect(drawer.$toolbar).toBeInstanceOf(HTMLDivElement);
-    expect(drawer.$toolbar.querySelectorAll('button, input').length).toEqual(12);
-  });
-
-  it('Init drawer with custom toolbar', () => {
-    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, {
-      defaultToolbar: false,
-      autoSave: false,
-    });
-    drawer.addToolbar();
-    drawer.addUndoBtn();
-    drawer.addRedoBtn();
-    drawer.addBrushBtn();
-    drawer.addEraserBtn();
-
-    expect(drawer.$toolbar).toBeInstanceOf(HTMLDivElement);
-    expect(drawer.$toolbar.querySelectorAll('button').length).toEqual(4);
+    expect(drawer.$toolbar.childElementCount).toEqual(12);
   });
 
   it('Init drawer with custom size', () => {
@@ -62,30 +49,5 @@ describe('Drawer', () => {
     expect(drawer.$canvas).toBeInstanceOf(HTMLCanvasElement);
     expect(drawer.$canvas.width).toEqual(200);
     expect(drawer.$canvas.height).toEqual(200);
-  });
-
-  it('Resize canvas', () => {
-    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
-
-    drawer.setSize(500, 600);
-    expect(drawer.$canvas.width).toEqual(500);
-    expect(drawer.$canvas.height).toEqual(600);
-    // check max-width update of toolbar
-    expect(drawer.$toolbar.style.maxWidth).toEqual('500px');
-  });
-
-  it('Update line width', () => {
-    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
-
-    drawer.setLineWidth(10);
-    expect(drawer.ctx.lineWidth).toEqual(10);
-  });
-
-  it('Change tool', () => {
-    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
-
-    drawer.changeTool('eraser');
-    expect(drawer.activeTool).toEqual('eraser');
-    expect(drawer.$eraserBtn?.classList.contains('active')).toEqual(true);
   });
 });
