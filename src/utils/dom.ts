fea05c203e65c2e214e5dcae29d66a626d300590
Fabien Winkler <fabien.winkler@outlook.fr>
2023-10-02 12:54:59 +0200

    ✨ add getScrollbarWidth util method

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,7 @@
 export function stringToHTMLElement<T>(string: string): T {
   return new DOMParser().parseFromString(string, 'text/html').body.firstChild as T;
 }
+
+export function getScrollbarWidth(): number {
+  return window.innerWidth - document.documentElement.clientWidth;
+}
