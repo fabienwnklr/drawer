9e2e57b39e0d82062171f4bc424b1e1bd8327dc8
Fabien Winkler <fabien.winkler@outlook.fr>
2023-10-03 14:09:05 +0200

    chore(vite): update vite conf deprecated warn

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,47 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import path from "node:path";
+import { defineConfig } from 'vite';
+import path from 'node:path';
 
 export default defineConfig({
   build: {
     minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: path.resolve(__dirname, "src/Drawer.ts"),
-      name: "Drawer",
+      entry: path.resolve(__dirname, 'src/Drawer.ts'),
+      name: 'Drawer',
       // the proper extensions will be added
-      fileName: "drawer",
-      formats: ["iife", "cjs", "es", "umd"],
+      fileName: 'drawer',
+      formats: ['iife', 'cjs', 'es', 'umd'],
     },
   },
   resolve: {
     alias: {
-      find: "~",
-      replacement: path.resolve(__dirname, "src"),
+      find: '~',
+      replacement: path.resolve(__dirname, 'src'),
     },
   },
   test: {
-    setupFiles: ["./vitest.setup.ts"],
-    environment: "jsdom",
-    threads: false,
-    deps: {
-      inline: ["vitest-canvas-mock"],
+    server: {
+      deps: {
+        inline: ['vitest-canvas-mock'],
+      },
     },
+    setupFiles: ['./vitest.setup.ts'],
+    environment: 'jsdom',
+    threads: false,
     // For this config, check https://github.com/vitest-dev/vitest/issues/740
     environmentOptions: {
       jsdom: {
-        resources: "usable",
+        resources: 'usable',
       },
     },
     exclude: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/cypress/**",
-      "**/.{idea,git,cache,output,temp}/**",
-      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
-      "**/e2e/**",
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/e2e/**',
     ],
   },
 });
