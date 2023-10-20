e245821402c7a224bac72aaddf7c81b55610fbd9
Fabien Winkler <fabien.winkler@outlook.fr>
2023-10-20 15:29:28 +0200

    feat: add bgColor to settings modal

--- src/ui/SettingsModal.ts
+++ src/ui/SettingsModal.ts
@@ -1,5 +1,7 @@
+import Coloris from '@melloware/coloris';
 import type { Drawer } from '../Drawer';
 import { Modal } from './Modal';
+import { GithubIcon } from '../icons/github';
 
 export class SettingsModal extends Modal {
   filled: boolean;
@@ -7,6 +9,7 @@ export class SettingsModal extends Modal {
   guides: boolean;
   opacity: number;
   xor: boolean;
+  bgColor: string;
 
   drawer: Drawer;
   $fillSettingInput!: HTMLInputElement;
@@ -14,15 +17,17 @@ export class SettingsModal extends Modal {
   $guidesSettingInput!: HTMLInputElement;
   $opacitySettingInput!: HTMLInputElement;
   $xorSettingInput!: HTMLInputElement;
+  $bgCologSettingInput!: HTMLInputElement;
 
   constructor(drawer: Drawer) {
-    super(drawer, { title: "Settings" });
+    super(drawer, { title: 'Settings' });
     this.drawer = drawer;
     this.filled = drawer.options.fill;
     this.grid = drawer.options.grid;
     this.guides = drawer.options.guides;
     this.opacity = drawer.options.opacity;
     this.xor = drawer.options.xor;
+    this.bgColor = drawer.options.bgColor;
     this.fill();
     this._setupSelectors();
     this._initEvents();
@@ -34,6 +39,12 @@ export class SettingsModal extends Modal {
   fill() {
     this.setBodyContent(/*html*/ `
       <ul class="drawer-modal-body-list">
+        <li class="drawer-modal-body-list-item">
+          <label for="setting-bgcolor-${this.drawer.options.id}">Background color (🚨 this removing all draw)</label>
+          <input tabindex="-1" class="btn" id="setting-bgcolor-${this.drawer.options.id}"  name="bgcolor-${
+            this.drawer.options.id
+          }" type="texr" data-coloris value="${this.bgColor}"/>
+        </li>
         <li class="drawer-modal-body-list-item">
           <label for="setting-opacity-${this.drawer.options.id}">Global opacity (0.1 / 1)</label>
           <input id="setting-opacity-${this.drawer.options.id}"  name="opacity-${
@@ -59,7 +70,9 @@ export class SettingsModal extends Modal {
           }" ${this.guides ? 'checked' : ''}>
         </li>
         <li class="drawer-modal-body-list-item">
-          <label for="setting-xor-${this.drawer.options.id}">XOR (<a href="https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation" target="_blank" />example</a>)</label>
+          <label for="setting-xor-${
+            this.drawer.options.id
+          }">XOR (<a href="https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation" target="_blank" />example</a>)</label>
           <input id="setting-xor-${this.drawer.options.id}" type="checkbox" name="xor-${this.drawer.options.id}" ${
             this.xor ? 'checked' : ''
           }>
@@ -67,10 +80,26 @@ export class SettingsModal extends Modal {
       </ul>
     `);
 
-    this.setFooterContent(/*html*/ `<small>Version ${this.drawer.VERSION}</small>`);
+    this.setFooterContent(
+      /*html*/ `<div class="drawer-modal-footer"><small>Version ${this.drawer.VERSION}</small><a class="link" target="_blank" title="Visit code source" href="https://github.com/fabwcie/drawer">${GithubIcon}</a></div>`
+    );
+
+    // Init input color
+    Coloris.init();
+    Coloris({
+      el: `#setting-bgcolor-${this.drawer.options.id}`,
+      theme: 'polaroid',
+      swatches: this.drawer.options.availableColor,
+      swatchesOnly: this.drawer.options.availableColorOnly,
+      formatToggle: !this.drawer.options.availableColorOnly,
+    });
   }
 
   private _setupSelectors() {
+    this.$bgCologSettingInput = this.$modalBody.querySelector(
+      `#setting-bgcolor-${this.drawer.options.id}`
+    ) as HTMLInputElement;
+
     this.$fillSettingInput = this.$modalBody.querySelector(
       `#setting-fill-${this.drawer.options.id}`
     ) as HTMLInputElement;
@@ -91,6 +120,10 @@ export class SettingsModal extends Modal {
   }
 
   private _initEvents() {
+    this.$bgCologSettingInput.addEventListener('change', () => {
+      this.drawer.setBgColor(this.$bgCologSettingInput.value);
+    });
+
     this.$fillSettingInput.addEventListener('change', () => {
       this.drawer.options.fill = this.$fillSettingInput.checked;
     });
