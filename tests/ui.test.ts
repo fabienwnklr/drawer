13eba2988c0fd41b28c5f339a4360ac5a44ced08
Fabien Winkler <fabien.winkler@outlook.fr>
2024-01-16 11:50:59 +0100

    test: add ui test

---
+++ tests/ui.test.ts
@@ -0,0 +1,73 @@
+import { describe, expect, it, beforeAll } from 'vitest';
+import { Drawer } from '../src/Drawer';
+import { Blob } from 'node:buffer';
+import { confirmModalDefaultOpts } from '../src/constants';
+import { ConfirmModal } from '../src/ui/ConfirmModal';
+
+declare global {
+  interface Window {
+    Blob: any;
+  }
+}
+
+beforeAll(() => {
+  window.Blob = Blob; // use Node.js Blob instead of Jsdom's Blob
+});
+
+describe('Ui', () => {
+  document.body.innerHTML = `<div id="test"></div>`;
+
+  it('Open default confirm modal', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
+
+    const confirm = new ConfirmModal(drawer);
+    confirm.show();
+    expect(confirm.isVisible()).toBe(true);
+    expect(confirm.$modalBody.textContent).toBe(confirmModalDefaultOpts.message);
+    expect(confirm.$cancelBtn.textContent).toBe(confirmModalDefaultOpts.cancelLabel);
+    expect(confirm.$confirmBtn.textContent).toBe(confirmModalDefaultOpts.confirmLabel);
+  });
+
+  it('Open custom confirm modal', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
+
+    const confirm = new ConfirmModal(drawer, {
+      message: 'Test',
+      cancelLabel: 'Cancel test',
+      confirmLabel: 'Confirm test',
+    });
+
+    expect(confirm.isVisible()).toBe(false);
+    confirm.show();
+    expect(confirm.isVisible()).toBe(true);
+    expect(confirm.$modalBody.textContent).toBe('Test');
+    expect(confirm.$cancelBtn.textContent).toBe('Cancel test');
+    expect(confirm.$confirmBtn.textContent).toBe('Confirm test');
+  });
+
+  it('Not open clear confirm modal without change', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
+
+    drawer.toolbar.$clearBtn?.dispatchEvent(new Event('click'));
+
+    expect(drawer.clearModal).toBe(undefined);
+  });
+
+  it('Open clear confirm modal', () => {
+    const drawer = new Drawer(document.body.querySelector('#test') as HTMLDivElement, { autoSave: false });
+
+    // draw on canvas for can display confirm clear modal
+    const img = new Image();
+    img.src = './img/test.png';
+    img.onload = () => {
+      drawer.ctx.drawImage(img, 0, 0);
+      drawer.toolbar.$clearBtn?.dispatchEvent(new Event('click'));
+      console.log(drawer.clearModal);
+      expect(drawer.clearModal).toBeInstanceOf(ConfirmModal);
+      expect(drawer.clearModal.isVisible()).toBe(true);
+      expect(drawer.clearModal.$cancelBtn?.innerText).toBe(confirmModalDefaultOpts.cancelLabel);
+      expect(drawer.clearModal.$cancelBtn?.innerText).toBe(confirmModalDefaultOpts.cancelLabel);
+      expect(drawer.clearModal.$confirmBtn?.innerText).toBe(confirmModalDefaultOpts.confirmLabel);
+    };
+  });
+});
